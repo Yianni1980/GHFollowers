fe5320dde61f77d291be38392ea788d32c0f63e8
Yianni1980 <ioagian1980@gmail.com>
2023-06-17 13:36:37 +0300

    Added Utilities group

---
+++ GHFollowers/Utilities/GFError.swift
@@ -0,0 +1,16 @@
+//
+//  GFError.swift
+//  GHFollowers
+//
+//  Created by ioannis on 17/6/23.
+//
+
+import Foundation
+enum GFError: String,Error {
+    case invalidUsername="This username created an invalid request. Please try again."
+    case unableToComplete =  "Unable to complete your request. Please check your internet connection"
+    case invalidResponse =  "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+    case unableToFavorite = "there was an error favoriting this user please try again"
+    case alreadyInFavorites = "You 've already favorited this user. You must really like them!!"
+}
