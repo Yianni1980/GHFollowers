fd92cf3e3cf3746c914df8878c3b3a58fbd69c2c
Yianni1980 <ioagian1980@gmail.com>
2023-06-17 19:43:59 +0300

    Added custom ViewControllers

--- GHFollowers/customViews/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
+++ GHFollowers/customViews/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -5,4 +5,22 @@
 //  Created by ioannis on 17/6/23.
 //
 
-import Foundation
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+        
+    }
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(background: .systemGreen, title: "Get Followers")
+        
+    }
+    override func actionButtonTapped() {
+        delegate.didTapGetFollowers(for: user)
+    }
+}
+
--- GHFollowers/customViews/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
+++ GHFollowers/customViews/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -5,4 +5,21 @@
 //  Created by ioannis on 17/6/23.
 //
 
-import Foundation
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+        
+    }
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(background: .systemPurple, title: "GitHub Profile")
+        
+    }
+    override func actionButtonTapped() {
+        delegate.didTapGitHubProfile(for: user)
+    }
+}
